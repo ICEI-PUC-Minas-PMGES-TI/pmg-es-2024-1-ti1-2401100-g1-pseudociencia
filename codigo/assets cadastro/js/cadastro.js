349bf227bd2ece8561e82b414a33a75a4525be1b
unknown <joaovslopes20@gmail.com>
2024-05-10 11:12:51 -0300

    adição do assets cadastro, com o js, css e html da página de cadastro

---
+++ codigo/assets cadastro/js/cadastro.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("form");
+const nomeInput = document.querySelector("#nome");
+const numeroInput = document.querySelector("#numero");
+const emailInput = document.querySelector("#email");
+const senhaInput = document.querySelector("#senha");
+const senha2Input = document.querySelector("#senha2");
+
+function salvar() {
+    if (nomeInput.value && numeroInput.value && emailInput.value && senhaInput.value && senha2Input.value){
+        if (senhaInput.value === senha2Input.value) {
+        let pessoa = {
+            id: obterID(),
+            nome: nomeInput.value.trim(),
+            numero: numeroInput.value.trim(),
+            email: emailInput.value.trim(),
+            senha: senhaInput.value.trim()
+        };
+
+        let pessoas = JSON.parse(localStorage.getItem("pessoas")) || [];
+        pessoas.push(pessoa);
+        localStorage.setItem("pessoas", JSON.stringify(pessoas));
+
+
+        form.reset();
+        window.location.href = "cadastro.html";
+    }
+    else {
+        let errado = document.getElementById("errado");
+        errado.classList.add("erro");
+    }
+}
+else {
+    let erro = document.getElementById("erro");
+        erro.classList.add("erro");
+}
+};
+
+function obterID() {
+    let id = parseInt(localStorage.getItem("id")) || 0;
+    id += 1;
+    localStorage.setItem("id", id);
+    return id;
+};
